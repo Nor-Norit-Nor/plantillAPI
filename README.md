dde04e293be62198668510999f20f3e62006ac65
ngc <nora.gonzalo@gmail.com>
2021-03-06 10:31:28 +0100

    FIXED js, ADD resources to readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # plantillAPI
 
-sessionStorage: https://developer.mozilla.org/es/docs/Web/API/Window/sessionStorage
-fetch: https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Using_Fetch
+// --- --- RECURSOS UTILIZADOS--- --- //
+_LOCAL STORAGE_: https://developer.mozilla.org/es/docs/Web/API/Window/localStorage
+_FETCH_: https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Using_Fetch
+_JSON.PARSE_: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+_JSON.STRINGIFY_: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+_QUERYSELECTOR_: https://developer.mozilla.org/es/docs/Web/API/Element/querySelector
+_CREATE ELEMENT_: https://developer.mozilla.org/es/docs/Web/API/Document/createElement
+_CHILDNODE REMOVE_: https://developer.mozilla.org/es/docs/Web/API/ChildNode/remove
+_SET ATTRIBUTE_: https://developer.mozilla.org/es/docs/Web/API/Element/setAttribute
+_APPEND CHILD_: https://developer.mozilla.org/es/docs/Web/API/Node/appendChild
+_CREATE TEXT NODE_: https://developer.mozilla.org/es/docs/Web/API/Document/createTextNode
+_ADD EVENT LISTENER_: https://developer.mozilla.org/es/docs/Web/API/EventTarget/addEventListener
--- index.js
+++ index.js
@@ -7,26 +7,10 @@ let containerResults = document.querySelector(".containerResults");
 
 
 //FUNCIONES
-// function fetchTo(character) {
-//     if(character !== "") {
-//       fetch(`https://rickandmortyapi.com/api/character/?name=${character}`)
-//         .then(response => response.json())
-//         .then(data => {
-//           sessionStorage.setItem(`save-${character}`, JSON.stringify(data))
-//           console.log(data)
-//           return data
-//         })
-//         .then(data => 
-//             data.results.map(element => paintResults(element)
-//             //recuerda que en data.map debe hacerse si el resultado del fetch es un array de objetos. 
-//           ))
-//     }
-//   }
 function fetchTo(character){
   if(character !== ""){
     fetch(`https://rickandmortyapi.com/api/character/?name=${character}`)
           .then(response => response.json())
-          // .then(data => { return data})
           .then(data => 
             data.results.map(element => paintResults(element), saveData(data)
             //recuerda que en data.map debe hacerse si el resultado del fetch es un array de objetos. 
@@ -35,97 +19,61 @@ function fetchTo(character){
 }
 //GUARDAMOS DATA EN LOCALSTORAGE
 const saveData =(data)=>{
-const storedData = JSON.stringify(data)
-localStorage.setItem("storedData", storedData)
+const storedData = JSON.stringify(data);
+localStorage.setItem("storedData", storedData);
 }
 
+//IR ATRÁS
 const goBack = () => {
-  let containerD= document.querySelector('.containerDetails')
-  // console.log(containerD)
+  let containerD= document.querySelector('.containerDetails');
+
   containerD.remove();
+
   let containerResults = document.createElement("div");
-  containerResults.setAttribute('class', 'containerResults')
+  containerResults.setAttribute('class', 'containerResults');
   wrapper.appendChild(containerResults);
-  // paintResults(restore)
   
-  const storedData = JSON.parse(localStorage.getItem("storedData")) 
-  // const results = storedData.results
- storedData.results.map(element =>  paintResults(element))
- console.log('he ido atrás', storedData)
-
-
-
+  const storedData = JSON.parse(localStorage.getItem("storedData"));
+  storedData.results.map(element =>  paintResults(element));
 }
 
 //PINTAMOS RESULTADOS
   function paintResults(element) {
-    let containerResults = document.querySelector(".containerResults")
+      let containerResults = document.querySelector(".containerResults");
+
       let result = document.createElement("div");
       containerResults.appendChild(result);
+
       let text = document.createElement("p");
       let content = document.createTextNode(element.name);
       text.appendChild(content);
       containerResults.appendChild(text);
-      text.addEventListener('click', () => paintDetails(element));
-      // saveData(data);
-      console.log('element', text)
 
+      text.addEventListener('click', () => paintDetails(element));
   }
 
   buttonSearch.addEventListener('click', function() {
-    fetchTo (input.value) 
+    fetchTo (input.value); 
   })
 
+  //PINTAMOS DETALLE
   function paintDetails(element) {
-        containerResults.remove();    
+        containerResults.remove();   
+
         let containerDetails = document.createElement("div");
         containerDetails.setAttribute("class", "containerDetails");
         wrapper.appendChild(containerDetails);
+
         let picture = document.createElement("img"); 
         picture.setAttribute("src", element.image);
-   
         containerDetails.appendChild(picture);
+
         let backBtn = document.createElement('button');
         backBtn.setAttribute("class", "btn-back");
         backBtn.setAttribute('content', 'BACK');
         backBtn.textContent = 'BACK';
         containerDetails.appendChild(backBtn);
 
-        backBtn.addEventListener('click', goBack)
+        backBtn.addEventListener('click', goBack);
   }
 
-//   function goBack(){
-//     console.log('paso por back')
-// let storageSaved;
-// if(sessionStorage.getItem(`save-${character}`)){
-//   storageSaved = JSON.parse(localStorage.getItem(`save-${character}`))
- 
-
-// }else{
-// storageSaved=[]
-// }
-//       paintResults(storageSaved)
-//   }
-
-//   const Delete = () => {
-
-//     let nodes = document.querySelectorAll(".search, .container, #backButton")
-//     nodes.forEach(el => el.remove())
-
-// }
-
-
-
-
-
-// const reset = () => {
-
-//     const storedSearch = JSON.stringify("")
-//     localStorage.setItem("storedSearch", storedSearch)
-//     input.value = ""
-//     deletePreviousNodes()
-
-// }
-
-
-  
